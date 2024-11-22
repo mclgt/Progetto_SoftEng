425fe3f8475fd973eefc9d65e979bd9f0ef73df4
camja <camja@172.19.236.45>
2024-11-22 15:12:04 +0100

    ciao

--- Rubrica_SoftEng/src/main/java/com/mycompany/rubrica_softeng/MainProva.java
+++ Rubrica_SoftEng/src/main/java/com/mycompany/rubrica_softeng/MainProva.java
@@ -16,7 +16,7 @@ public class MainProva {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Prima stampa di prova!");
-        
+        System.out.printn("ciao");
         System.out.println("Controllo email Reb");
     }
     
