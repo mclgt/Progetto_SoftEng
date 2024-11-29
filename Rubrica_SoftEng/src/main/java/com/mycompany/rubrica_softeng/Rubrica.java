bc880fd4089d440fdadae65bb0cbc57be76525e0
Michela <m.gaeta29@studenti.unisa.it>
2024-11-29 14:40:00 +0100

    Creazione classe Main Rubrica

---
+++ Rubrica_SoftEng/src/main/java/com/mycompany/rubrica_softeng/Rubrica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.rubrica_softeng;
+
+/**
+ *
+ * @author miche
+ */
+public class Rubrica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
