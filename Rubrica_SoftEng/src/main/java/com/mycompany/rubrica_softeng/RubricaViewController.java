3d87f1f85971c33580e5989bca42e544f12cf1c6
Michela <m.gaeta29@studenti.unisa.it>
2024-12-02 14:43:51 +0100

    Aggiunta metodi principali al controller

--- Rubrica_SoftEng/src/main/java/com/mycompany/rubrica_softeng/RubricaViewController.java
+++ Rubrica_SoftEng/src/main/java/com/mycompany/rubrica_softeng/RubricaViewController.java
@@ -6,6 +6,8 @@ package com.mycompany.rubrica_softeng;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -22,5 +24,25 @@ public class RubricaViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void importContact(ActionEvent event) {
+    }
+
+    @FXML
+    private void exportContact(ActionEvent event) {
+    }
+
+    @FXML
+    private void addContact(ActionEvent event) {
+    }
+
+    @FXML
+    private void modifyContact(ActionEvent event) {
+    }
+
+    @FXML
+    private void removeContact(ActionEvent event) {
+    }
     
 }
