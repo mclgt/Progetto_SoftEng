f88d054f17ebdb7f0ebbeb97eda40eb2e78aea98
Antonio <a.caliendo31@studenti.unisa.it>
2024-12-04 09:56:03 +0100

    Inizializzazione Scene

--- Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/Main/Rubrica.java
+++ Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/Main/Rubrica.java
@@ -18,7 +18,10 @@ public class Rubrica extends Application{
 
     @Override 
     public void start(Stage stage) throws Exception{
-
+        Parent root= FXMLLoader.load(getClass().getResource("RubricaView.fxml"));
+        Scene scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     /**
      * @param args the command line arguments
