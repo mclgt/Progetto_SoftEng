c118634a701fda7558efe4a171bdc00d974e7973
miche <miche@172.19.213.216>
2024-12-04 09:10:19 +0100

    Organizzazione package

--- Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/contatti/Rubrica.java
+++ Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/contatti/Rubrica.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.mycompany.rubrica_softeng;
+package it.unisa.diem.softeng.contatti;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/contatti/RubricaViewController.java
+++ Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/contatti/RubricaViewController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.mycompany.rubrica_softeng;
+package it.unisa.diem.softeng.contatti;
 
 import java.net.URL;
 import java.util.ResourceBundle;
