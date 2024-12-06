4ae79f642873c9397595b9264dabd3ec20fae0f9
miche <miche@172.19.213.216>
2024-12-06 15:45:16 +0100

    Aggiornamento commenti doxygen

--- Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/contatti/Contatto.java
+++ Rubrica_SoftEng/src/main/java/it/unisa/diem/softeng/contatti/Contatto.java
@@ -8,11 +8,12 @@ package it.unisa.diem.softeng.contatti;
     * @file Contatto.java
     * @brief Classe creata per gestire un contatto base. La classe è composta da vari attributi quali: nome(String), cognome(String), 
     * numero (NumeroTelefonico, vedi file NumeroTelefonico.java) e email(Email, vedi file Email.java).
+    * @invariant gli attributi nome e cognome non possono mai essere entrambi vuoti, almeno uno dei due deve contenere un valore non nullo.
     * 
     * @author Autore
     * @date data
     * 
- */
+    */
 
 public class Contatto {
     private String nome;
@@ -24,31 +25,25 @@ public class Contatto {
     * @brief Costruttore del generico oggetto Contatto con il quale si possono inizializzare nome, cognome, numero e email
     * @pre Bisogna aggiungere almeno un nome o un cognome
     * @post Viene generato un contatto con almeno un nome o un cognome 
-    * @invariant descrizione invarianti
     * 
     * @param nome[in]
     * @param cognome[in]
     * @param numero[in]: oggetto contenente al più tre numeri
     * @param email[in]: oggetto contenete al più tre email
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
-    public Contatto(String nome, String cognome, NumeroTelefonico numero, Email email){
+    */
+    public Contatto(String nome, String cognome, String[] numero, String[] email){
+      
     }
     
     
  /**
     * @brief Permette di ricavare l'attributo nome da un contatto
     * @pre Deve esistere un contatto
-    * @post Ritorna il nome del contatto su cui viene richiamato il metodo
-    * @invariant descrizione invarianti
     * @param nome[out]
     * 
-    * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    * @return nome contatto
+    */
     public String getNome(){
         return null;
     }
@@ -56,13 +51,10 @@ public class Contatto {
   /**
     * @brief Permette di ricavare l'attributo cognome da un contatto
     * @pre Deve esistere un contatto
-    * @post Ritorna il cognome del contatto su cui viene richiamato il metodo
-    * @invariant descrizione invarianti
     * @param cognome[out]
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    * @return cognome contatto
+    */
     public String getCognome(){
         return null;
     }
@@ -70,13 +62,10 @@ public class Contatto {
   /**
     * @brief Permette di ricavare l'attributo numero da un contatto
     * @pre Deve esistere un contatto
-    * @post Ritorna il/i numero/i del contatto su cui viene richiamato il metodo
-    * @invariant descrizione invarianti
     *@param numeri[out]: oggetto che contiene al più tre numeri
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    * @return numeri associati al contatto
+    */
     public NumeroTelefonico getNumero() {
         return null;
     }
@@ -84,13 +73,10 @@ public class Contatto {
   /**
     * @brief Permette di ricavare l'attributo email da un contatto
     * @pre Deve esistere un contatto
-    * @post Ritorna la/le email del contatto su cui viene richiamato il metodo
-    * @invariant descrizione invarianti
     * @param email[out]: oggetto che contiene al più tre mail
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    * @return email associate al contatto
+    */
     public Email getEmail() {
         return null;
     }
@@ -99,38 +85,29 @@ public class Contatto {
     * @brief Permette di modificare l'attributo nome da un contatto
     * @pre Deve esistere un contatto
     * @post Modifica il nome del contatto su cui viene chiamato il metodo
-    * @invariant descrizione invarianti
     * @param nome[in]
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    */
     public void setNome(String nome) {
     }
 
-/**
+  /**
     * @brief Permette di modificare l'attributo cognome da un contatto
     * @pre Deve esistere un contatto
     * @post Modifica il cognome del contatto su cui viene chiamato il metodo 
-    * @invariant descrizione invarianti
     * @param cognome[in]
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    */
     public void setCognome(String cognome) {
     }
 
- /**
+  /**
     * @brief Permette di modificare l'attributo numero da un contatto
     * @pre Deve esistere un contatto
     * @post Modifica il/i numero/i del contatto su cui viene chiamato il metodo 
-    * @invariant descrizione invarianti
     * @param numero[][in]
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    */
     public void setNumero(NumeroTelefonico numero) {
     }
 
@@ -138,12 +115,9 @@ public class Contatto {
     * @brief Permette di modificare l'attributo email da un contatto
     * @pre Deve esistere un contatto
     * @post Modifica la/le email del contatto su cui viene chiamato il metodo
-    * @invariant descrizione invarianti
     * @param email[][in]
     * 
-    * @return specifica cosa restituisce la funzione
-    * @throws Exception 
- */
+    */
     public void setEmail(Email email) {
     }
     
